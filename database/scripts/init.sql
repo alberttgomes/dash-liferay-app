7e54456a957160e304c65ee14c3ea095c07dc2ed
Albert Gomes <albert.gomes@liferay.com>
2023-04-01 21:41:01 -0300

    Fix query to init database and user

--- database/scripts/init.sql
+++ database/scripts/init.sql
@@ -1,4 +1,7 @@
+DROP DATABASE lportal;
+
 CREATE DATABASE lportal;
+USE lportal;
 
 CREATE USER 'albertlportal'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY 'pass';
 
@@ -6,7 +9,5 @@ GRANT ALL ON lportal.* TO 'albertlportal'@'127.0.0.1';
 
 /* Make sure the privileges are installed */
 FLUSH PRIVILEGES;
-
-USE lportal;
 
 ALTER USER 'albertlportal'@'localhost' IDENTIFIED BY 'pass';
