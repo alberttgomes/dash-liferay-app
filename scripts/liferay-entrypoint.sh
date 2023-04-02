18a14ad2c9299d60a71357cdcdc1693b8907bfd2
Albert Gomes <albert.gomes@liferay.com>
2023-04-02 11:56:07 -0300

    Create script to move .jar of artifacts to deploy

---
+++ scripts/liferay-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ARTIFACTS="$LIFERAY_HOME/artifacts"
+
+# Copy artifacts to into deploy folder
+if [ -d $ARTIFACTS ]; then
+    if [ -z "$(ls -A $ARTIFACTS)" ] 
+    then
+        echo "No files found in the $ARTIFACTS folder"
+    else
+        echo "Copy files from $ARTIFACTS folder to into deploy folder"
+        mv -v $ARTIFACTS/*jar $LIFERAY_HOME/deploy/
+    fi
+fi
+
+# Run Liferay
+/opt/liferay/tomcat/bin/catalina.sh run
