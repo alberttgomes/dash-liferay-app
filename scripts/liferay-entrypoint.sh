894a85ab4d30f18f4418da030b79d2e887a9a91f
Albert Gomes <albert.gomes@liferay.com>
2023-04-08 15:06:33 -0300

    Add mv command in script to build container portal

--- scripts/liferay-entrypoint.sh
+++ scripts/liferay-entrypoint.sh
@@ -5,14 +5,33 @@ ARTIFACTS="$LIFERAY_HOME/artifacts"
 # Copy artifacts to into deploy folder
 if [ -d $ARTIFACTS ] 
 then
+    
+    # Check if was copied successfully to artifact
     if [ -z "$(ls -A $ARTIFACTS)" ] 
     then
         echo "No files found in the $ARTIFACTS folder"
     else
-        echo "Copy files from $ARTIFACTS folder to into deploy folder"
+        #Copy files from artifact to deploy
+        echo "Building bundles directory into Liferay container..."
+
+        echo "Moving files from $ARTIFACTS to into deploy folder"
         mv -v $ARTIFACTS/*jar $LIFERAY_HOME/deploy/
+
+        # Copying artifacts to document library and deploying the customer .jar on bundles
+        DOCUMENT_LIBRARY=$ARTIFACTS/document_library
+
+        if [ -d $DOCUMENT_LIBRARY ]
+        then 
+                echo "Moving document library from $ARTIFACTS to data folder..."
+                # Remove defualt document library Liferay
+                rm -r $$LIFERAY_HOME/data/document_library/*
+                # Move document library from artifacts to document library data folder
+                mv $DOCUMENT_LIBRARY $LIFERAY_HOME/data/document_library/
+        else 
+                echo "Directory $DOCUMENT_LIBRARY not found"
+        fi
     fi
 fi
 
-# Run Liferay
+# # Run Liferay
 /opt/liferay/tomcat/bin/catalina.sh run
