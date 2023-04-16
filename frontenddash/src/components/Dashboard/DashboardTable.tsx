4c91fd9d092cf058395d42666b18b55992985867
Albert Gomes <albert.gomes@liferay.com>
2023-04-16 11:49:41 -0300

    Complete dashboard table component

--- frontenddash/src/components/Dashboard/DashboardTable.tsx
+++ frontenddash/src/components/Dashboard/DashboardTable.tsx
@@ -3,8 +3,7 @@ import ClayTable from '@clayui/table';
 
 type DashboardTableInfoTypes = {
     quantity: number
-    companyName: string;
-    selected: boolean;
+    id: number
     status: string;
     titleProduct: string;
 }
@@ -19,23 +18,56 @@ export const DashboardTableInfo = ({dashboardItems}: IPropsDashboardTableInfo) =
         <ClayTable 
             className="dsh-dashboard-table-container"
         >
-             <ClayTable.Head>
-                    <ClayTable.Cell headingCell>
-                        <div className='dsh-dashboard-table-header'>
-                            <span>
-                                 Product Name
-                            </span>
-                        </div>
-                    </ClayTable.Cell>
+                <ClayTable.Head>
+                        <ClayTable.Cell headingCell>
+                            <div className='dsh-dashboard-table-header'>
+                                <span>
+                                    Product Name
+                                </span>
+                            </div>
+                        </ClayTable.Cell>
 
-                    <ClayTable.Cell headingCell>
-                        <div className='dsh-dashboard-table-header'>
-                            <span>
-                                 Product Name
-                            </span>
-                        </div>
-                    </ClayTable.Cell>
-             </ClayTable.Head>
+                        <ClayTable.Cell headingCell>
+                            <div className='dsh-dashboard-table-header'>
+                                <span>
+                                    Product Id
+                                </span>
+                            </div>
+                        </ClayTable.Cell>
+
+                        <ClayTable.Cell headingCell>
+                            <div className='dsh-dashboard-table-header'>
+                                <span>
+                                    Product Quantity
+                                </span>
+                            </div>
+                        </ClayTable.Cell>
+
+                        <ClayTable.Cell headingCell>
+                            <div className='dsh-dashboard-table-header'>
+                                <span>
+                                    Product Status
+                                </span>
+                            </div>
+                        </ClayTable.Cell>
+                </ClayTable.Head>
+
+                <ClayTable.Body>
+                    {dashboardItems.map((data, index) => {
+                                return (
+                                        <ClayTable.Row key={index}>
+                                                <ClayTable.Cell> {data.titleProduct} </ClayTable.Cell>
+
+                                                <ClayTable.Cell> {data.id} </ClayTable.Cell>
+                                               
+                                                <ClayTable.Cell> {data.quantity} </ClayTable.Cell>
+                                                   
+                                                <ClayTable.Cell> {data.status} </ClayTable.Cell>
+                                                   
+                                        </ClayTable.Row>
+                                );
+                    })}
+                </ClayTable.Body>
         </ClayTable>
     );
 }
